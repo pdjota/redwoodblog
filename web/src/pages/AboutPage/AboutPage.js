2ccde6a3852d71e8c8bb8422d77821d7e29541a9
Pablo Dejuan <pablo@pablo.pm>
2021-05-30 20:28:43 +0100

    use layouts for home and about

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -3,23 +3,10 @@ import { Link, routes } from '@redwoodjs/router'
 const AboutPage = () => {
   return (
     <>
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>Acerca de</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
         <p>
           Este sitio ha sido creado para probar mi destreza con Redwood: "¡Mirad
 Mi obra, poderosos! ¡Desesperad!"
         </p>
-        <Link to={routes.home()}>Regresar al inicio</Link>
-      </main>
     </>
   )
 }
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -2,19 +2,7 @@ import { Link, routes } from '@redwoodjs/router'
 
 const HomePage = () => {
   return (
-    <>
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>Acerca de</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>Inicio</main>
-    </>
+    <main>Inicio</main>
   )
 }
 
